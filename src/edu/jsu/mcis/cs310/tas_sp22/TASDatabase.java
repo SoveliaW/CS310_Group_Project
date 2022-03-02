9f614fde3e8c216e00a38a3f8922265b1fa42808
Sovelia Williams <swilliams43@stu.jsu.edu>
2022-03-01 21:44:34 -0600

    Merge origin/master
    Conflicts:
    	src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java

--- src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
@@ -13,15 +13,12 @@ public class TASDatabase {
     
     private final Connection connection;
     
-    
     public TASDatabase(String username, String password, String address) {
-        
         this.connection = openConnection(username, password, address);
-        
-        
     }
     String results =null;
     
+        /*Badge*/
     
     public Badge getBadge(String badgeid) {
         String id = null, des = null;
@@ -42,57 +39,25 @@ public class TASDatabase {
                 }
             }
         }
-        catch (Exception e) { e.printStackTrace(); }
+        catch (Exception e) { 
+            e.printStackTrace();
+        }
+        
         Badge result = new Badge(id, des);
         return result;
     } 
     
-      public Employee getEmployee(Badge badge_id) {       //getEmployee that takes a Badge id as a parameter
-        ArrayList<String> keys = new ArrayList<String>();
-        HashMap <String, String> empl = new HashMap <>() ;
-        String id = badge_id.getId();
-
-        try{
-         String query= "SELECT * FROM tas_sp22_v1.employee WHERE id =?;";
-         PreparedStatement pstmt = connection.prepareStatement(query);
-         pstmt.setString(1,id);
-         boolean pstmtExe = pstmt.execute();
-         
-         if(pstmtExe){
-            ResultSet resultset = pstmt.getResultSet(); 
-              
-            while(resultset.next()){
-                ResultSetMetaData metadata = resultset.getMetaData();
-                
-                int columnCount = metadata.getColumnCount();
-            
-                    for (int i = 1; i <= columnCount; ++i) {
-
-                    keys.add(metadata.getColumnLabel(i));
-                   
-                    }
-                     for (int i = 1; i <= columnCount; ++i) {
-                  
-                    Object value = resultset.getObject(i);
-                    empl.put(keys.get(i - 1), String.valueOf(value));
-                    }  
-                }
-            }
-        }
-        catch(Exception e) {
-         e.printStackTrace(); 
-        }
-        Employee hmap = new Employee(empl);
-        return hmap;
-    } 
-    public Employee getEmployee(int id) {       //getEmployee that takes an int id as a parameter
+        /*Employee*/       
+    
+    public Employee getEmployee(int id) {       
         ArrayList<String> keys = new ArrayList<String>();
         HashMap <String, String> empl = new HashMap <>() ;
 
         try{
-         String query= "SELECT * FROM tas_sp22_v1.employee WHERE id =?;";
+         String query= "SELECT * FROM employee WHERE id = ?;";
          PreparedStatement pstmt = connection.prepareStatement(query);
          pstmt.setInt(1,id);
+         
          boolean pstmtExe = pstmt.execute();
          
          if(pstmtExe){
@@ -100,30 +65,57 @@ public class TASDatabase {
               
             while(resultset.next()){
                 ResultSetMetaData metadata = resultset.getMetaData();
-                
                 int columnCount = metadata.getColumnCount();
             
                     for (int i = 1; i <= columnCount; ++i) {
-
-                    keys.add(metadata.getColumnLabel(i));
-                   
+                        keys.add(metadata.getColumnLabel(i));
                     }
-                     for (int i = 1; i <= columnCount; ++i) {
-                  
-                    Object value = resultset.getObject(i);
-                    empl.put(keys.get(i - 1), String.valueOf(value));
+                    
+                    for (int i = 1; i <= columnCount; ++i) {
+                        Object value = resultset.getObject(i);
+                        empl.put(keys.get(i - 1), String.valueOf(value));
                     }  
                 }
             }
         }
         catch(Exception e) {
-         e.printStackTrace(); 
+            e.printStackTrace(); 
         }
+        
         Employee hmap = new Employee(empl);
         return hmap;
+    }
+    
+    public Employee getEmployee(Badge id) {       
+        String badgeid = id.getId();
+        int id_int = 0;
+
+        try {
+            String query = "SELECT * FROM employee WHERE badgeid = ?;";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(2, badgeid);
+            
+            boolean ptExe = pstmt.execute();
+            
+            if (ptExe) {
+                ResultSet resultset = pstmt.executeQuery();
+                
+                while(resultset.next()){
+                    badgeid = resultset.getString(2);
+                    id_int = resultset.getInt(1);
+                }
+            }
+        }
+        catch (Exception e) { 
+            e.printStackTrace();
+        }
+        
+        return getEmployee(id_int);
     } 
     
-    public Punch  getPunch(int id) {       //getEmployee that takes a Badge id as a parameter
+            /*Punch*/       
+    
+    public Punch  getPunch(int id) {
         ArrayList<String> keys = new ArrayList<String>();
         HashMap <String, String> Pun = new HashMap <>() ;
         
@@ -131,25 +123,23 @@ public class TASDatabase {
          String query= "SELECT * FROM tas_sp22_v1.employee WHERE id =?;";
          PreparedStatement pstmt = connection.prepareStatement(query);
          pstmt.setInt(1,id);
+         
          boolean pstmtExe = pstmt.execute();
          
          if(pstmtExe){
             ResultSet resultset = pstmt.getResultSet(); 
               
             while(resultset.next()){
-                ResultSetMetaData metadata = resultset.getMetaData();
-                
+                ResultSetMetaData metadata = resultset.getMetaData();  
                 int columnCount = metadata.getColumnCount();
             
                     for (int i = 1; i <= columnCount; ++i) {
-
-                    keys.add(metadata.getColumnLabel(i));
-                   
+                        keys.add(metadata.getColumnLabel(i));
                     }
-                     for (int i = 1; i <= columnCount; ++i) {
-                  
-                    Object value = resultset.getObject(i);
-                    Pun.put(keys.get(i - 1), String.valueOf(value));
+                    
+                    for (int i = 1; i <= columnCount; ++i) {
+                        Object value = resultset.getObject(i);
+                        Pun.put(keys.get(i - 1), String.valueOf(value));
                     }  
                 }
             }
@@ -157,38 +147,38 @@ public class TASDatabase {
         catch(Exception e) {
          e.printStackTrace(); 
         }
+        
         Punch hmap = new Punch(Pun);
         return hmap;
     } 
+    
+            /*Shift*/       
    
     public Shift getShift(int id) {       //getShift that takes an int id as a parameter
-       
         ArrayList<String> keys = new ArrayList<String>();
         HashMap <String, String> shif =new HashMap <>() ;
         
         try{
-         String query= "Select *FROM shift WHERE id = ?;";
-         PreparedStatement pstmt = connection.prepareStatement(query);
-         pstmt.setInt(1,id);
-        boolean pstmtExe = pstmt.execute();
+            String query= "Select *FROM shift WHERE id = ?;";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1,id);
+            
+            boolean pstmtExe = pstmt.execute();
         
-         if(pstmtExe){
+        if(pstmtExe){
             ResultSet resultset = pstmt.getResultSet(); 
               
             while(resultset.next()){
                 ResultSetMetaData metadata = resultset.getMetaData();
-                
                 int columnCount = metadata.getColumnCount();
             
                     for (int i = 1; i <= columnCount; ++i) {
-
-                    keys.add(metadata.getColumnLabel(i));
-                   
+                        keys.add(metadata.getColumnLabel(i));
                     }
-                     for (int i = 1; i <= columnCount; ++i) {
-                  
-                    Object value = resultset.getObject(i);
-                    shif.put(keys.get(i - 1), String.valueOf(value));
+                    
+                    for (int i = 1; i <= columnCount; ++i) {
+                        Object value = resultset.getObject(i);
+                        shif.put(keys.get(i - 1), String.valueOf(value));
                     }  
                 }
             }
@@ -196,92 +186,76 @@ public class TASDatabase {
         catch(Exception e) {
          e.printStackTrace(); 
         }
+        
         Shift hmap = new Shift(shif);
         return hmap;
         }
     
-    public Shift getShift(Badge badge_id) {       //getShift that takes a Badge id as a parameter
-       
-        ArrayList<String> keys = new ArrayList<String>();
-        HashMap <String, String> shif =new HashMap <>() ;
-        int id = Integer.parseInt(badge_id.getId());
-        
-        try{
-         String query= "Select *FROM shift WHERE id = ?;";
-         PreparedStatement pstmt = connection.prepareStatement(query);
-         pstmt.setInt(1,id);
-        boolean pstmtExe = pstmt.execute();
-        
-         if(pstmtExe){
-            ResultSet resultset = pstmt.getResultSet(); 
-              
-            while(resultset.next()){
-                ResultSetMetaData metadata = resultset.getMetaData();
-                
-                int columnCount = metadata.getColumnCount();
-            
-                    for (int i = 1; i <= columnCount; ++i) {
+    public Shift getShift(Badge id) {       //getShift that takes a Badge id as a parameter
+        String badgeid = id.getId();
+        int id_int = 0;
 
-                    keys.add(metadata.getColumnLabel(i));
-                   
-                    }
-                     for (int i = 1; i <= columnCount; ++i) {
-                  
-                    Object value = resultset.getObject(i);
-                    shif.put(keys.get(i - 1), String.valueOf(value));
-                    }  
+        try {
+            String query = "SELECT * FROM employee WHERE badgeid = ?;";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(2, badgeid);
+            
+            boolean ptExe = pstmt.execute();
+            
+            if (ptExe) {
+                ResultSet resultset = pstmt.executeQuery();
+                
+                while(resultset.next()){
+                    badgeid = resultset.getString(2);
+                    id_int = resultset.getInt(1);
                 }
             }
         }
-        catch(Exception e) {
-         e.printStackTrace(); 
+        catch (Exception e) { 
+            e.printStackTrace();
         }
-        Shift hmap = new Shift(shif);
-        return hmap;
+        
+        return getShift(id_int);
         }
     
-    
+        /*Connection*/
     
     public boolean isConnected() {
-
         boolean result = false;
         
         try {
             
             if ( !(connection == null) )
-                
                 result = !(connection.isClosed());
-            
         }
-        catch (Exception e) { e.printStackTrace(); }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         
-        return result;
-        
+        return result;   
     }
+    
     private Connection openConnection(String u, String p, String a) {
-        
         Connection c = null;
         
-        if (a.equals("") || u.equals("") || p.equals(""))
-            
+        if (a.equals("") || u.equals("") || p.equals("")){
             System.err.println("*** ERROR: MUST SPECIFY ADDRESS/USERNAME/PASSWORD BEFORE OPENING DATABASE CONNECTION ***");
+        }
         
         else {
         
             try {
-
                 String url = "jdbc:mysql://" + a + "/tas_sp22_v1?autoReconnect=true&useSSL=false&zeroDateTimeBehavior=EXCEPTION&serverTimezone=America/Chicago";
                 // System.err.println("Connecting to " + url + " ...");
 
                 c = DriverManager.getConnection(url, u, p);
-
             }
-            catch (Exception e) { e.printStackTrace(); }
-           
+            catch (Exception e) {
+                e.printStackTrace();
+            }  
         }
         
-        return c;
-         
+        return c;    
     }
 }
  
