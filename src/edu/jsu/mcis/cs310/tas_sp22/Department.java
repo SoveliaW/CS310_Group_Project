13281718483801d44bf267e4c53e0075d4ef9d47
jessecastillo <6.24jcastillorolltide@gmail.com>
2022-03-07 13:10:52 -0600

    Adding Department Class

---
+++ src/edu/jsu/mcis/cs310/tas_sp22/Department.java
@@ -0,0 +1,32 @@
+package edu.jsu.mcis.cs310.tas_sp22;
+import java.util.HashMap;
+import java.time.LocalDate;
+
+
+public class Department {
+    private String description;
+    private int id, terminalid;
+    
+    public Department(HashMap <String, String> params) {
+        
+        this.description = params.get("description");
+        this.id = Integer.parseInt(params.get("id"));
+        this.terminalid = Integer.parseInt(params.get("terminalid"));
+        
+        
+        
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTerminalid() {
+        return terminalid;
+    }
+    
+}
