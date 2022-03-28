73b3a089a3d6520400bfc231926026e256ce27ac
jessecastillo <6.24jcastillorolltide@gmail.com>
2022-03-28 02:17:51 -0500

    Feature 5 Testing

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,8 +1,12 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +23,32 @@ public class TAS {
     }
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift)
     {
+
+        try{
+            String query = "SELECT * FROM tas_sp22_v1.event WHERE timestamp =?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1,id);
+         
+            boolean pstmtExe = pstmt.execute();
+
+            if (pstmtExe) {
+
+                ResultSet resultset = pstmt.getResultSet();
+
+                while(resultset.next()) {
+                    params.put("id", String.valueOf(id));
+                    params.put("terminalid", String.valueOf(resultset.getInt("terminalid")));
+                    params.put("eventtypeid",String.valueOf(resultset.getInt("eventtypeid")));
+                    params.put("timestamp", resultset.getTimestamp("timestamp").toLocalDateTime().toString());
+                    params.put("badgeid", resultset.getString("badgeid"));
+                   
+                }
+            }
+          
+        }
+        catch(Exception e) {
+            e.printStackTrace(); 
+        }
         
     }
 }
