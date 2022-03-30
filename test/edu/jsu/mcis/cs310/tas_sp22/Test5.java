492a3c07b909006e4739c1081ff1d1956212a1f4
ccosper <ccosper@stu.jsu.edu>
2022-03-30 12:54:53 -0500

    Test5 added

---
+++ test/edu/jsu/mcis/cs310/tas_sp22/Test5.java
@@ -0,0 +1,18 @@
+package edu.jsu.mcis.cs310.tas_sp22;
+
+import java.util.ArrayList;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class Test5 {
+    
+    public Test5() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
