c6615ec4d77445eef797a066e56c19ba07817a36
jessecastillo <6.24jcastillorolltide@gmail.com>
2022-04-01 13:33:23 -0500

    Test 5, 3 tests passing

--- test/edu/jsu/mcis/cs310/tas_sp22/Test5.java
+++ test/edu/jsu/mcis/cs310/tas_sp22/Test5.java
@@ -7,12 +7,114 @@ import static org.junit.Assert.*;
 
 public class Test5 {
     
-    public Test5() {
+    private TASDatabase db;
+    
+    @Before
+    public void setup() {
+        
+       db = new TASDatabase("tasuser", "PASSWORD", "localhost");
+        
+    }
+    
+        @Test
+        public void testMinutesAccruedShift1Weekday() {
+		
+        /* Get Punch */
+        
+        Punch p = db.getPunch(759);
+        Badge b = p.getBadge();
+        Shift s = db.getShift(b);
+        
+        ArrayList<Punch> dailypunchlist = db.getDailyPunchList(b, p.getOriginalTimestamp().toLocalDate());
+        
+        for (Punch punch : dailypunchlist) {
+            punch.adjust(s);
+        }
+		
+        /* Compute Pay Period Total */
+        
+        int m = TAS.calculateTotalMinutes(dailypunchlist, s);
+		
+        /* Compare to Expected Value */
+        
+        assertEquals(480, m);
+        
+    }
+    
+    
+    @Test
+    public void testMinutesAccruedShift1WeekdayWithTimeout() {
+		
+        /* Get Punch */
+        
+        Punch p = db.getPunch(272);
+        Badge b = p.getBadge();
+        Shift s = db.getShift(b);
+        
+        ArrayList<Punch> dailypunchlist = db.getDailyPunchList(b, p.getOriginalTimestamp().toLocalDate());
+        
+        for (Punch punch : dailypunchlist) {
+            punch.adjust(s);
+        }
+		
+        /* Compute Pay Period Total */
+        
+        int m = TAS.calculateTotalMinutes(dailypunchlist, s);
+		
+        /* Compare to Expected Value */
+        
+        assertEquals(0, m);
+        
+    }
+    
+     @Test
+    public void testMinutesAccruedShift1Weekend() {
+		
+        /* Get Punch */
+        
+        Punch p = db.getPunch(5216);
+        Badge b = p.getBadge();
+        Shift s = db.getShift(b);
+        
+        ArrayList<Punch> dailypunchlist = db.getDailyPunchList(b, p.getOriginalTimestamp().toLocalDate());
+        
+        for (Punch punch : dailypunchlist) {
+            punch.adjust(s);
+        }
+		
+        /* Compute Pay Period Total */
+        
+        int m = TAS.calculateTotalMinutes(dailypunchlist, s);
+		
+        /* Compare to Expected Value */
+        
+        assertEquals(600, m);
+        
+    }
+    
+        @Test
+    public void testMinutesAccruedShift2Weekday() {
+		
+        /* Get Punch */
+        
+        Punch p = db.getPunch(5020);
+        Badge b = p.getBadge();
+        Shift s = db.getShift(b);
+        
+        ArrayList<Punch> dailypunchlist = db.getDailyPunchList(b, p.getOriginalTimestamp().toLocalDate());
+        
+        for (Punch punch : dailypunchlist) {
+            punch.adjust(s);
+        }
+        
+        /* Compute Pay Period Total */
+        
+        int m = TAS.calculateTotalMinutes(dailypunchlist, s);
+		
+        /* Compare to Expected Value */
+        
+        assertEquals(600, m);
+        
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
